c43a281c1c18d0a234c3beea6a223170d49d1d0e
Malith Anjana <it19051376@my.sliit.lk>
2022-07-25 14:39:13 +0530

    create: implement the login function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import AdminMain from "./pages/AdminMain";
+import Login from './pages/Login';
+import StudentMain from "./pages/StudentMain";
 
 function App() {
+  const user = localStorage.getItem("token");
+  const userRole = localStorage.getItem("role");
+
   return (
-    <div className="App">
-      <h1>Hallo</h1>
-    </div>
+    <Routes>
+			{user && 
+         userRole === 'admin'
+         && <Route path="/" exact element={<AdminMain/>}
+      />}
+      {user && 
+         userRole === 'student'
+         && <Route path="/" exact element={<StudentMain/>}
+      />}
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
   );
 }
 
---
+++ src/pages/StudentMain/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentMain = () => {
+  return (
+    <div><h1>Student</h1></div>
+  )
+}
+
+export default StudentMain
